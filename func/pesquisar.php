58e3fb4211e5a7438fab9a5d39cc21ec23b0c986
cybelle123 <gusaodbr@gmail.com>
2023-05-13 18:14:16 -0300

    errad0

--- func/pesquisar.php
+++ func/pesquisar.php
@@ -3,15 +3,7 @@
 
 function search($resultado){
 require('pdo.inc.php');
-if ($timestamp = strtotime($resultado)) {
-    $sql = $pdo->prepare('SELECT * FROM documentos WHERE DATE(data_upload) = :resultado AND usuarios_id = :id_usuario');
-    $resultado =  date('Y-m-d', $timestamp);
-    $sql->bindParam(':resultado', $resultado);
-    $sql->bindParam(':id_usuario', $_SESSION['id_usuario']);
 
-$sql->execute();
-return $sql->fetchAll(PDO::FETCH_ASSOC);
-  } else {
     $sql = $pdo->prepare('SELECT * FROM documentos WHERE nome LIKE :resultado AND usuarios_id = :id_usuario');
     $resultado = "%$resultado%";
     $sql->bindParam(':resultado', $resultado);
@@ -19,4 +11,3 @@ return $sql->fetchAll(PDO::FETCH_ASSOC);
 $sql->execute();
 return $sql->fetchAll(PDO::FETCH_ASSOC);
   }
-}
